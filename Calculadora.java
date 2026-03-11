2cb89bfde155d5d70faeb303f037d9c20f2394c7
Lizbeth <pierinagdlv@gmail.com>
2026-03-10 21:01:17 -0500

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,22 @@
+package EjercicioParticipacion;
+
+public class Calculadora {
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public double restar(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double dividir(double a, double b) throws DivisionPorCeroException {
+        if (b == 0) {
+            throw new DivisionPorCeroException("Error: No se puede dividir entre cero.");
+        }
+        return a / b;
+    }
+}
